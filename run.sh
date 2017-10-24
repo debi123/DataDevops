64cec610056aff3ded58b6db2d377a9b2aad3c6a
debi123 <debarati.sinharoy@emc.com>
2017-10-24 15:06:07 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
 
-psql --username "$POSTGRES_USER" -c "select * from 
- 
+psql --username "$POSTGRES_USER" -c "select table_name from information_schema.tables where table_type = 'BASE TABLE' and table_schema = 'public'"
 
