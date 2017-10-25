83df98943548e08ec1e70b2c773e65415277758e
debi123 <debarati.sinharoy@emc.com>
2017-10-25 15:00:40 +0530

    Update customers.sql

--- table_scripts/customers.sql
+++ table_scripts/customers.sql
@@ -3,3 +3,5 @@
 -- DROP TABLE customers;
 
 CREATE TABLE my_first_table (first_column text,second_column integer);
+insert into my_first_table (first_column,second_column) values (emc,121);
+select count(*) from my_first_table;
