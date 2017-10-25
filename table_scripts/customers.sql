a916c052f62a422f9f9eb24b226e64b464673335
debi123 <debarati.sinharoy@emc.com>
2017-10-25 14:56:29 +0530

    Update customers.sql

--- table_scripts/customers.sql
+++ table_scripts/customers.sql
@@ -2,21 +2,4 @@
 
 -- DROP TABLE customers;
 
-CREATE TABLE customers
-(
-  customer_id integer,
-  date_created character varying(20),
-  account_num character varying(20),
-  priority integer,
-  manager_acc character varying(20),
-  request_number character varying(20),
-  call_type text,
-  final_code text,
-  state text
-)
-WITH (
-  OIDS=FALSE
-)
-DISTRIBUTED BY (customer_id);
-ALTER TABLE customers
-  OWNER TO gpadmin;
+CREATE TABLE my_first_table (first_column text,second_column integer);
