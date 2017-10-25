36cc4c3f14b5807acfaca064f7fee7c1bf84711c
debi123 <debarati.sinharoy@emc.com>
2017-10-25 12:16:30 +0530

    Update list_git_repo.sh

--- table_scripts/list_git_repo.sh
+++ table_scripts/list_git_repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ls -l
 echo "Listed source directory structure"
-#cp ./gpdb-unit-test/table_scripts/*.sql ./gpdb-unit-test-output
+cp *.sql ../gpdb-unit-test-output
 #cd ./gpdb-unit-test-output
-#ls -l ../gpdb-unit-test-output
+ls -l ../gpdb-unit-test-output
