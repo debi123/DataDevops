f57bc5dde6866fcde4deeb0d41a71c4cb5d340d1
debi123 <debarati.sinharoy@emc.com>
2017-10-25 14:32:58 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,33 @@
-#!/bin/bash
+#!/bin/sh
 
-psql --username "$POSTGRES_USER" -c "select table_name from information_schema.tables where table_type = 'BASE TABLE' and table_schema = 'public'"
+ls
 
+cp customers.sql /home
+chmod 777 customers.sql
+
+cd /home
+mkdir postgres
+chmod 777 postgres
+
+cd /usr/local
+mkdir postgres
+chmod 777  postgres
+chown postgres postgres
+
+
+##cd /usr/lib/postgresql/10/bin/
+su - postgres -c '/usr/lib/postgresql/10/bin/initdb -D /usr/local/postgres'
+
+su - postgres -c '/usr/lib/postgresql/10/bin/pg_ctl start -D /usr/local/postgres -l serverlog'
+
+##su - postgres -c '/usr/lib/postgresql/10/bin/psql -c "CREATE schema trial" '
+
+##su - postgres -c '/usr/lib/postgresql/10/bin/psql -c "CREATE TABLE trial.weather (city integer, name integer)" '
+
+##su - postgres -c '/usr/lib/postgresql/10/bin/psql -c "insert into trial.weather (city,name) values (1,121) " '
+
+su - postgres -c '/usr/lib/postgresql/10/bin/psql -a -f "/home/customers.sql"'
+
+#su - postgres -c '/usr/lib/postgresql/10/bin/psql -c "select * from trial.weather" '
+
+exit;
