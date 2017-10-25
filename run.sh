10a1386b349ec6f4264536554ca7ded4b0d3a1a9
debi123 <debarati.sinharoy@emc.com>
2017-10-25 09:37:00 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
+
+chmod 777
 
 psql --username "$POSTGRES_USER" -c "select table_name from information_schema.tables where table_type = 'BASE TABLE' and table_schema = 'public'"
 
