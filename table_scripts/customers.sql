e6a8c727d76a65763df65c2af4b4abb7a4f22c81
debi123 <debarati.sinharoy@emc.com>
2017-10-26 10:56:23 +0530

    Update customers.sql

--- table_scripts/customers.sql
+++ table_scripts/customers.sql
@@ -1,8 +1,7 @@
 -- Table: customers
 
 -- DROP TABLE customers;
-EXEC SQL WHENEVER SQLERROR STOP;
 
 CREATE TABLE my_first_table (first_column text,second_column integer);
-insert into tmy_first_table (first_column,second_column) values ('emc',121);
+insert into my_first_table (first_column,second_column) values ('emc',121);
 select count(*) from my_first_table;
