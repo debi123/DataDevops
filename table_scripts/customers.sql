fda5220c7063ee479feabba75609caea2254733f
debi123 <debarati.sinharoy@emc.com>
2017-10-26 11:48:21 +0530

    Update customers.sql

--- table_scripts/customers.sql
+++ table_scripts/customers.sql
@@ -2,6 +2,11 @@
 
 -- DROP TABLE customers;
 
-CREATE TABLE my_first_table (first_column text,second_column integer);
-insert into my_first_table (first_column,second_column) values ('emc',121);
-select count(*) from my_first_table;
+--CREATE TABLE my_first_table (first_column text,second_column integer);
+--insert into my_first_table (first_column,second_column) values ('emc',121);
+--select count(*) from my_first_table;
+
+CREATE schema trial_gp;
+CREATE TABLE trial_gp.weather (city integer, name varchar(10));
+insert into trial_gp.weather (city,name) values (1,'aaa');
+insert into trial_gp.weather (city,name) values (2,'a1');
