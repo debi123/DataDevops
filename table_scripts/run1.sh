a64a03eafa83d254da69e8e56f74f5941fc3783d
debi123 <debarati.sinharoy@emc.com>
2017-11-09 08:43:07 +0530

    Update run1.sh

--- table_scripts/run1.sh
+++ table_scripts/run1.sh
@@ -5,11 +5,11 @@ pwd
 ls -lrt
 # cd /home
 # java -version
-sudo add-apt-repository ppa:openjdk-r/ppa
-sudo apt-get update
-java -version
-# sudo apt-get install -y default-jre
-sudo apt-get install -y default-jdk
+# sudo add-apt-repository ppa:openjdk-r/ppa
+# sudo apt-get update
+# java -version
+# # sudo apt-get install -y default-jre
+# sudo apt-get install -y default-jdk
 # sudo apt-get install -y openjdk-8-jre
 # sudo apt-get install -y openjdk-8-jdk
 # su -c "yum install java-1.8.0-openjdk"
@@ -17,6 +17,8 @@ java -version
 whereis java
 whereis jvm
 whereis java-7-openjdk-amd64
+sudo apt-get -y update
+sudo apt-get install -y openssh-server
 # sudo apt-get install default-jre
 # sudo apt-get install openjdk-8-jdk
 # sudo update-alternatives --config java
